3db2cdffc7c03ff3e5989ea40c0e1b79bd6bbbfc
Jacob Hein <jacobhein1892@gmail.com>
2021-01-13 18:56:14 +0000

    play.js updated

---
+++ play.js
@@ -0,0 +1,19 @@
+const net = require('net')
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+  conn.on('data', function(data) {
+    console.log('Message from server: ', data)
+  });
+  return conn; 
+}
+
+
+
+console.log('Connecting...');
+connect(); 
