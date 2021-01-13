ef3ecdd3f047487dee22c1cfaf8ff9d6b47f46ad
Jacob Hein <jacobhein1892@gmail.com>
2021-01-13 21:34:56 +0000

    moving markers

--- client.js
+++ client.js
@@ -11,6 +11,10 @@ const connect = function() {
     console.log('Connection established')
     conn.write('Name: JTH')
   })
+//   conn.on('connect', function(){
+//     conn.write('Move: up')
+//     conn.write('Move: up')
+// })
   
   conn.on('data', function(data) {
     console.log('Message from server: ', data)
@@ -18,5 +22,9 @@ const connect = function() {
   return conn; 
 }
 
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
 
 module.exports = connect;  
